79863dfa939b6eba2af86e8cce763adae940760c
Sadashiv88 <143304783+Sadashiv88@users.noreply.github.com>
2026-08-10 19:44:48 +0530

    Time: 9 ms (17.71%) | Memory: 46.5 MB (29.69%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,15 +1,15 @@
 class Solution {
       public int solve(int i, int j, int[] nums, int[][] dp) {
-        if (i == j) {
-            return nums[i];
+        if (i >j ) {
+            return 0;
         }
 
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
 
-        int pickLeft = nums[i] - solve(i + 1, j, nums, dp);
-        int pickRight = nums[j] - solve(i, j - 1, nums, dp);
+        int pickLeft = nums[i] +Math.min(solve(i+2,j,nums,dp),solve(i+1,j-1,nums,dp));
+        int pickRight = nums[j] +Math.min(solve(i,j-2,nums,dp),solve(i+1,j-1,nums,dp));
 
         return dp[i][j] = Math.max(pickLeft, pickRight);
        
@@ -23,6 +23,12 @@ class Solution {
             }
         }
 
-        return solve(0, n - 1, nums, dp) >= 0;
+        int sum=0;
+        for(int it:nums){
+            sum+=it;
+        }
+        int s=solve(0,n-1,nums,dp);
+        if(sum/2 >s) return false;
+        return true;
     }
 }
